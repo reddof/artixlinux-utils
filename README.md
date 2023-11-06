a09f7e1932a92c1147a74cbee8cc5140f506504a
reddof <reddof@gmail.com>
2023-11-06 22:09:19 +0700

    update

---
+++ README.md
@@ -0,0 +1,40 @@
+# archlinux-utils
+
+
+
+Didalam Script ini saya kumpulkan berbagai commands , diantaranya adalah 
+Install Arch Linux, Install DE, Install Third Party packages, 
+Install packages yang biasa kita gunakan sehari hari, Packages Updater, dll. 
+Tinggal Jalankan dan ikuti petunjuknya.
+
+
+
+1. Jalankan Script ini dengan cara ketik di terminal :
+```
+sudo ./start.sh
+```
+2. Apabila anda menjalankan script ini pada waktu instalasi pastikan anda
+   sudah terhubung ke Intenet, dan sudah mengatur partisi serta mount point.
+3. Pada waktu post install yaitu setelah chroot, anda bisa menemukan script
+   ini di /archlinux-utilities/start.sh
+```
+cd /archlinux-utils
+```
+```
+sudo ./start.sh
+```
+4. Begitu juga setelah reboot anda bisa menemukan script ini di
+   /archlinux-utilities/start.sh
+```
+cd /archlinux-utils
+```
+```
+sudo ./start.sh
+```
+
+Dalam repository ini ada folder dan files yang bukan milik saya, saya melampirkan folder dan files tersebut hanya untuk keperluan backup dan restore settings dan files saya. 
+
+
+credit .oh-my-zsh : https://ohmyz.sh/ semua hak dan kepemilikan adalah atas nama https://ohmyz.sh/
+
+Tekan titik dua dan q (:q) untuk keluar vim.
---
+++ start.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+DIR=$(pwd)
+SUB_DIR="$(pwd)/sub"
+
+chmod -x -R $DIR/*
+mkdir -p /mnt/artixlinux-utils
+cp -R $DIR/* /mnt/artixlinux-utils
+cp -R $DIR/.* /mnt/artixlinux-utils
+
+clear
+
+error () { clear
+
+read -p "
+
+Input yang anda masukkan salah !
+
+" input
+	case $input in
+		*) return
+		;;
+	esac
+}
+
+echo "
+################################################################################
+##                   ARTIX LINUX UTILITIES script by Reddof                   ##
+################################################################################
+##                                  MAIN MENU                                 ##
+##                                                                            ##
+##  1. Install Artix Linux Runit ke UEFI System                               ##
+##  2. Package Management                                                     ##
+##  3. Remove                                                                 ##
+##  4. README                                                                 ##
+##  q. Keluar                                                                 ##
+##                                                                            ##
+##                                                                            ##
+################################################################################
+"
+
+read -p "
+
+Silakan masukkan pilihan anda :
+
+" mm
+	case $mm in
+		1) $SUB_DIR/./inst
+		;;
+		2) $SUB_DIR/./pac-man
+		;;
+		3) $SUB_DIR/./remove
+		;;
+		4) vim $DIR/README.md
+		;;
+		[Qq]*) clear
+		exit
+		;;
+		*) error
+		;;
+	esac
+
+$DIR/./start.sh
